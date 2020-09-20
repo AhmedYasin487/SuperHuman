a4f0967098fc6583461f51f7819aba5984457d78
AhmedYasin487 <coolahmed21@example.com>
2020-09-19 23:42:59 -0700

    first commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 class UserCreateForm(UserCreationForm):
@@ -8,5 +9,22 @@ class UserCreateForm(UserCreationForm):
     class Meta :
         model = User
         fields =['username','email','password1','password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email__iexact=email).exists():
+           raise forms.ValidationError('Email is already register')
+        return email
+
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Display Name'
+        self.fields['email'].label = 'Email Address'
+
+
+
+     
+
+
     
     
